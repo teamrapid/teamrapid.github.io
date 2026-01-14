bbcff8cabf2b85ebb4e54bee387059243143ab98
kevinschaper <kevinschaper>
2026-01-14 17:46:13 +0000

    Update redirects

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 // GENERATED AUTOMATICALLY, DO NOT EDIT
-let list = "W10="; // empty array base64 encoded
+let list = "W3siZnJvbSI6ImNoYXRyb29tIiwidG8iOiJodHRwczovL3pvb20udXMvai8xMjM0NWFiY2RlZiJ9LHsiZnJvbSI6Imdvb2dsZWRvYyIsInRvIjoiaHR0cHM6Ly9kb2NzLmdvb2dsZS5jb20vZG9jdW1lbnQvZC8xMjM0NWFiY2RlZiJ9XQ=="; // empty array base64 encoded
 
 list = JSON.parse(window.atob(list));
 
